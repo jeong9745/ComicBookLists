9b986d3933971e1dec04c60d21df6371fd395dd7
jsy <jsy_0405@hanmail.net>
2024-02-24 19:35:23 +0900

    v.2

--- mysql.js
+++ mysql.js
@@ -36,7 +36,7 @@ app.get('/', (request,response)=> {
     });
 });
 
-
+//도서 입력 폼 제공, 새로운 도서를 추가할 수 있는 폼을 클라이언트에게 띄움.
 app.get('/create', (request,response)=> {
     fs.readFile('insertNewBook.html','utf-8',(error, data) => {
         if (error) throw error;
@@ -44,7 +44,7 @@ app.get('/create', (request,response)=> {
     });
 });
 
-
+//도서정보를 입력하면 입력된 도서 정보를 데이터베이스에 추가
 app.post('/create', (request,response)=> {
     const body = request.body;
     connection.query('INSERT INTO books (genre, name, writer, releasedate) VALUE (?, ?, ?, ?)', [body.genre, body.name, body.writer, body.releasedate], ()=>{
@@ -52,7 +52,7 @@ app.post('/create', (request,response)=> {
     });
 });
 
-
+//도서 수정 폼 제공, 폼은 modify.html 파일에 정의되어 있음.
 app.get('/modify/:id', (request,response)=> {
     fs.readFile('modify.html','utf-8',(error, data) => {
         connection.query('SELECT * from books WHERE number =?', [request.params.id], (error, results)=>{
@@ -65,6 +65,7 @@ app.get('/modify/:id', (request,response)=> {
     });
 });
 
+//도서정보를 수정하면, 수정된 도서 정보를 데이터베이스에 업데이트
 app.post('/modify/:id', (request,response)=> {
     const body = request.body;
     connection.query('UPDATE books SET genre = ?, name = ?, writer = ? WHERE number = ?', [body.genre, body.name, body.writer, request.params.id], (error, results)=>{
@@ -73,6 +74,7 @@ app.post('/modify/:id', (request,response)=> {
     });
 });
 
+//도서 삭제
 app.get('/delete/:id', (request,response)=> {
     connection.query('DELETE FROM books where number=?', [request.params.id], ()=>{
         response.redirect('/');
